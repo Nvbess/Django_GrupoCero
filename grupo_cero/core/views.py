059392dc9ecb85d04e41bdad57936bcaa06c7f26
Julio Orellana <130018600+Nvbess@users.noreply.github.com>
2024-04-19 19:29:06 -0400

    Grupo_Cero

--- grupo_cero/core/views.py
+++ grupo_cero/core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     	return render(request, 'core/index.html')
+
+def contacto(request):
+    	return render(request, 'core/contacto.html')
