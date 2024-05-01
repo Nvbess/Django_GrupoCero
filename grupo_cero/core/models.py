daf03862c434b92125202fff9c50bdd155541152
Julio Orellana <130018600+Nvbess@users.noreply.github.com>
2024-05-01 01:07:15 -0400

    Grupo_cero

--- grupo_cero/core/models.py
+++ grupo_cero/core/models.py
@@ -3,18 +3,18 @@ from django.db import models
 # Create your models here.
 
 class TipoObra(models.Model):
-    descripcion = models.CharField(max_length=50)
+    tipo_obra = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.descripcion
+        return self.tipo_obra
 
 class Obra(models.Model):
     nombre_obra = models.CharField(max_length=200)
-    autor = models.CharField(max_length=20)
-    fecha = models.DateField()
-    descripcion = models.CharField(max_length=500)
+    autor = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=1000)
     valor = models.IntegerField(default=0)
-    vendido = models.BooleanField(default=True)
+    vendido = models.BooleanField(default=False)
+    imagen = models.ImageField(upload_to='static/core/assets/img')
     tipo = models.ForeignKey(TipoObra, on_delete=models.PROTECT)
     
     def __str__(self):
